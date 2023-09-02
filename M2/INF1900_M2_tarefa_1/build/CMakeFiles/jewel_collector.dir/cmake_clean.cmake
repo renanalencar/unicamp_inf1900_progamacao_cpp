58c20d560e24370ccbce26715db1c6f2e5a72e16
Renan Costa Alencar <renan.costaalencar@gmail.com>
2023-09-02 10:42:33 -0300

    feat: Added new M2_Lab1

---
+++ M2/INF1900_M2_tarefa_1/build/CMakeFiles/jewel_collector.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jewel_collector.dir/jogo.cpp.obj"
+  "CMakeFiles/jewel_collector.dir/jogo.cpp.obj.d"
+  "jewel_collector.exe"
+  "jewel_collector.exe.manifest"
+  "jewel_collector.pdb"
+  "libjewel_collector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jewel_collector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
