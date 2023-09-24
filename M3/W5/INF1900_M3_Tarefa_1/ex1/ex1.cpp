3d1a085d0b6a3d813ad5e05358727348cadd403c
Renan Costa Alencar <renan.costaalencar@gmail.com>
2023-09-23 23:59:50 -0300

    feat: Add inf1900_m3_a1

--- M3/W5/INF1900_M3_Tarefa_1/ex1/ex1.cpp
+++ M3/W5/INF1900_M3_Tarefa_1/ex1/ex1.cpp
@@ -1,5 +1,12 @@
-// inf1900_m3_a1.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
+/**
+ * @file ex1.cpp
+ *
+ * @brief Classe com função main para execução dos pontos da atividade.
+ *
+ * @author Rafael Taveira / Renan Alencar
+ * Contact: rafael.t@sidi.org.br / renan.a@sidi.org.br
+ *
+ */
 
 #include <iostream>
 #include <filesystem>
@@ -7,6 +14,8 @@
 namespace fs = std::filesystem;
 
 int main() {
+    setlocale(LC_ALL, "pt_BR.UTF8");
+	
     // Criando diretórios
     fs::create_directory("diretorio1");
     fs::create_directories("diretorio2/diretorio3");
@@ -51,15 +60,3 @@ int main() {
 
     return 0;
 }
-
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
--- M3/W5/INF1900_M3_Tarefa_1/ex2/ex2.cpp
+++ M3/W5/INF1900_M3_Tarefa_1/ex2/ex2.cpp
@@ -1,157 +1,197 @@
-// ex2.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
+/**
+ * @file ex2.cpp
+ *
+ * @brief Classe com função main para execução dos pontos da atividade.
+ *
+ * @author Rafael Taveira / Renan Alencar
+ * Contact: rafael.t@sidi.org.br / renan.a@sidi.org.br
+ *
+ */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
+#include <locale> // Para configurar a codificação UTF-8
 
 using namespace std;
 
 struct Ferramenta {
-    int numeroRegistro;
-    string nomeFerramenta;
-    int quantidade;
-    double preco;
+	int numeroRegistro;
+	wstring nomeFerramenta;
+	int quantidade;
+	double preco;
 };
 
-// Função para inicializar o arquivo de acesso aleatório com registros vazios
-void inicializarArquivo(const string& arquivoNome) {
-    ofstream arquivo(arquivoNome, ios::binary | ios::out);
-
-    if (!arquivo.is_open()) {
-        cerr << "Erro ao abrir o arquivo." << endl;
-        exit(1);
-    }
-
-    Ferramenta ferramentaVazia = { 0, "", 0, 0.0 };
-
-    for (int i = 0; i < 100; ++i) {
-        arquivo.write(reinterpret_cast<char*>(&ferramentaVazia), sizeof(Ferramenta));
-    }
-
-    arquivo.close();
-}
-
 // Função para adicionar uma ferramenta ao arquivo
-void adicionarFerramenta(const string& arquivoNome, const Ferramenta& ferramenta, int numeroRegistro) {
-    fstream arquivo(arquivoNome, ios::binary | ios::in | ios::out);
+void adicionarFerramenta(const wstring& arquivoNome, const Ferramenta& ferramenta) {
+	wofstream arquivo(arquivoNome, ios::app);
+	arquivo.imbue(locale("pt_BR.utf8")); // Configura a codificação UTF-8
 
-    if (!arquivo.is_open()) {
-        cerr << "Erro ao abrir o arquivo." << endl;
-        exit(1);
-    }
+	if (!arquivo.is_open()) {
+		cerr << L"Erro ao abrir o arquivo." << endl;
+		exit(1);
+	}
 
-    // Posiciona o ponteiro de leitura/gravação no registro correto
-    arquivo.seekp(numeroRegistro * sizeof(Ferramenta), ios::beg);
-    arquivo.write(reinterpret_cast<const char*>(&ferramenta), sizeof(Ferramenta));
+	arquivo << ferramenta.numeroRegistro << L';'
+		<< ferramenta.nomeFerramenta << L';'
+		<< ferramenta.quantidade << L';'
+		<< ferramenta.preco << endl;
 
-    arquivo.close();
+	arquivo.close();
 }
 
 // Função para listar todas as ferramentas do arquivo
-void listarFerramentas(const string& arquivoNome) {
-    ifstream arquivo(arquivoNome, ios::binary | ios::in);
+void listarFerramentas(const wstring& arquivoNome) {
+	wifstream arquivo(arquivoNome);
+	arquivo.imbue(locale("pt_BR.utf8")); // Configura a codificação UTF-8
 
-    if (!arquivo.is_open()) {
-        cerr << "Erro ao abrir o arquivo." << endl;
-        exit(1);
-    }
+	if (!arquivo.is_open()) {
+		cerr << L"Erro ao abrir o arquivo." << endl;
+		exit(1);
+	}
 
-    Ferramenta ferramenta;
+	Ferramenta ferramenta;
+	vector<Ferramenta> ferramentas;
+	wstring linha;
 
-    while (arquivo.read(reinterpret_cast<char*>(&ferramenta), sizeof(Ferramenta))) {
-        if (ferramenta.numeroRegistro != 0) {
-            cout << "Número de Registro: " << ferramenta.numeroRegistro << endl;
-            cout << "Nome da Ferramenta: " << ferramenta.nomeFerramenta << endl;
-            cout << "Quantidade: " << ferramenta.quantidade << endl;
-            cout << "Preço: " << ferramenta.preco << endl << endl;
-        }
-    }
+	while (getline(arquivo, linha)) {
+		wstringstream ss(linha);
+		wstring campo;
 
-    arquivo.close();
+		getline(ss, campo, L';');
+		ferramenta.numeroRegistro = stoi(campo);
+
+		getline(ss, ferramenta.nomeFerramenta, L';');
+		getline(ss, campo, L';');
+		ferramenta.quantidade = stoi(campo);
+
+		getline(ss, campo);
+		ferramenta.preco = stod(campo);
+
+		ferramentas.push_back(ferramenta);
+	}
+
+	arquivo.close();
+
+	if (ferramentas.empty()) {
+		wcout << L"Nenhuma ferramenta encontrada no inventário." << endl;
+	}
+	else {
+		wcout << L"Lista de Ferramentas:" << endl;
+		for (const Ferramenta& f : ferramentas) {
+			wcout << L"Número de Registro: " << f.numeroRegistro << endl;
+			wcout << L"Nome da Ferramenta: " << f.nomeFerramenta << endl;
+			wcout << L"Quantidade: " << f.quantidade << endl;
+			wcout << L"Preço: " << f.preco << endl << endl;
+		}
+	}
 }
 
 // Função para excluir uma ferramenta com base no número de registro
-void excluirFerramenta(const string& arquivoNome, int numeroRegistro) {
-    Ferramenta ferramentaVazia = { 0, "", 0, 0.0 };
+void excluirFerramenta(const wstring& arquivoNome, int numeroRegistro) {
+	wifstream arquivoEntrada(arquivoNome);
+	wofstream arquivoTemp(L"temp.dat");
+	arquivoEntrada.imbue(locale("pt_BR.utf8")); // Configura a codificação UTF-8
+	arquivoTemp.imbue(locale("pt_BR.utf8")); // Configura a codificação UTF-8
 
-    fstream arquivo(arquivoNome, ios::binary | ios::in | ios::out);
+	if (!arquivoEntrada.is_open() || !arquivoTemp.is_open()) {
+		cerr << L"Erro ao abrir o arquivo." << endl;
+		exit(1);
+	}
 
-    if (!arquivo.is_open()) {
-        cerr << "Erro ao abrir o arquivo." << endl;
-        exit(1);
-    }
+	Ferramenta ferramenta;
+	bool encontrou = false;
+	wstring linha;
 
-    arquivo.seekp(numeroRegistro * sizeof(Ferramenta), ios::beg);
-    arquivo.write(reinterpret_cast<const char*>(&ferramentaVazia), sizeof(Ferramenta));
+	while (getline(arquivoEntrada, linha)) {
+		wstringstream ss(linha);
+		wstring campo;
 
-    arquivo.close();
+		getline(ss, campo, L';');
+		ferramenta.numeroRegistro = stoi(campo);
+
+		getline(ss, ferramenta.nomeFerramenta, L';');
+		getline(ss, campo, L';');
+		ferramenta.quantidade = stoi(campo);
+
+		getline(ss, campo);
+		ferramenta.preco = stod(campo);
+
+		if (ferramenta.numeroRegistro != numeroRegistro) {
+			arquivoTemp << ferramenta.numeroRegistro << L';'
+				<< ferramenta.nomeFerramenta << L';'
+				<< ferramenta.quantidade << L';'
+				<< ferramenta.preco << endl;
+		}
+		else {
+			encontrou = true;
+		}
+	}
+
+	arquivoEntrada.close();
+	arquivoTemp.close();
+
+	if (!encontrou) {
+		wcout << L"Ferramenta com o número de registro " << numeroRegistro << L" não encontrada." << endl;
+	}
+	else {
+		_wremove(arquivoNome.c_str());
+		_wrename(L"temp.dat", arquivoNome.c_str());
+		wcout << L"Ferramenta excluída com sucesso." << endl;
+	}
 }
 
 int main() {
-    const string arquivoNome = "hardware.dat";
+	wstring arquivoNome = L"hardware.dat";
 
-    // Inicializa o arquivo com registros vazios (apenas na primeira execução)
-    ifstream arquivoVerificador(arquivoNome);
-    if (!arquivoVerificador) {
-        inicializarArquivo(arquivoNome);
-    }
-    arquivoVerificador.close();
+	while (true) {
+		// Configurar a localização temporária para aceitar números com vírgula
+		locale loc("");
+		wcout.imbue(loc);
+		wcout << L"Escolha uma opção:" << endl;
+		wcout << L"1. Adicionar uma ferramenta" << endl;
+		wcout << L"2. Listar todas as ferramentas" << endl;
+		wcout << L"3. Excluir uma ferramenta" << endl;
+		wcout << L"4. Sair" << endl;
 
-    while (true) {
-        cout << "Escolha uma opção:" << endl;
-        cout << "1. Adicionar uma ferramenta" << endl;
-        cout << "2. Listar todas as ferramentas" << endl;
-        cout << "3. Excluir uma ferramenta" << endl;
-        cout << "4. Sair" << endl;
+		int escolha;
+		wcin >> escolha;
 
-        int escolha;
-        cin >> escolha;
+		if (escolha == 1) {
+			Ferramenta novaFerramenta;
+			wcout << L"Número de Registro: ";
+			wcin >> novaFerramenta.numeroRegistro;
+			wcin.ignore();
+			wcout << L"Nome da Ferramenta: ";
+			getline(wcin, novaFerramenta.nomeFerramenta);
+			wcout << L"Quantidade: ";
+			wcin >> novaFerramenta.quantidade;
+			wcout << L"Preço: ";
+			wcin.imbue(loc);
+			wcin >> novaFerramenta.preco;
 
-        if (escolha == 1) {
-            Ferramenta novaFerramenta;
-            cout << "Número de Registro: ";
-            cin >> novaFerramenta.numeroRegistro;
-            cout << "Nome da Ferramenta: ";
-            cin >> novaFerramenta.nomeFerramenta;
-            cout << "Quantidade: ";
-            cin >> novaFerramenta.quantidade;
-            cout << "Preço: ";
-            cin >> novaFerramenta.preco;
+			adicionarFerramenta(arquivoNome, novaFerramenta);
+			wcout << L"Ferramenta adicionada com sucesso." << endl;
+		}
+		else if (escolha == 2) {
+			listarFerramentas(arquivoNome);
+		}
+		else if (escolha == 3) {
+			int numeroRegistro;
+			wcout << L"Informe o número de registro da ferramenta a ser excluída: ";
+			wcin >> numeroRegistro;
 
-            adicionarFerramenta(arquivoNome, novaFerramenta, novaFerramenta.numeroRegistro);
-            cout << "Ferramenta adicionada com sucesso." << endl;
-        }
-        else if (escolha == 2) {
-            listarFerramentas(arquivoNome);
-        }
-        else if (escolha == 3) {
-            int numeroRegistro;
-            cout << "Informe o número de registro da ferramenta a ser excluída: ";
-            cin >> numeroRegistro;
+			excluirFerramenta(arquivoNome, numeroRegistro);
+		}
+		else if (escolha == 4) {
+			break;
+		}
+		else {
+			wcout << L"Opção inválida. Tente novamente." << endl;
+		}
+	}
 
-            excluirFerramenta(arquivoNome, numeroRegistro);
-            cout << "Ferramenta excluída com sucesso." << endl;
-        }
-        else if (escolha == 4) {
-            break;
-        }
-        else {
-            cout << "Opção inválida. Tente novamente." << endl;
-        }
-    }
-
-    return 0;
+	return 0;
 }
-
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
