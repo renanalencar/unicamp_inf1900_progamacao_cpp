c07d1a83baa822ec9de75efcef789531e0626d99
Renan Costa Alencar <renan.costaalencar@gmail.com>
2023-10-18 10:04:44 -0300

    feat: Implement assignments

--- M3/W8/INF1900_M3_Exercicio_2/main.cpp
+++ M3/W8/INF1900_M3_Exercicio_2/main.cpp
@@ -1,5 +1,96 @@
-#include <iostream>
+/**
+ * @file main.cpp
+ *
+ * @brief Classe com função main para execução dos pontos da atividade.
+ *
+ * @author Rafael Taveira / Renan Alencar
+ * Contact: rafael.t@sidi.org.br / renan.a@sidi.org.br
+ *
+ */
 
-int main(int, char**){
-    std::cout << "Hello, from INF_1900_M3_Exercicio_2!\n";
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <exception>
+
+namespace fs = std::filesystem;
+
+// Classe derivada de std::exception para representar a exceção de arquivo não-texto
+class NotATextFileException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "O arquivo não é um arquivo de texto válido.";
+    }
+};
+
+int main() {
+    std::string filename;
+
+    // Passo 1: Solicitar ao usuário o nome do arquivo de texto
+    std::cout << "Digite o nome do arquivo de texto: ";
+    std::cin >> filename;
+
+    try {
+        // Passo 2: Verificar se o arquivo existe
+        if (!fs::exists(filename)) {
+            throw fs::filesystem_error("Arquivo não encontrado", fs::errc::no_such_file_or_directory);
+        }
+
+        // Passo 3: Tentar abrir o arquivo
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            throw std::ios_base::failure("Não foi possível abrir o arquivo.");
+        }
+
+        // Passo 4: Tratar exceções durante a abertura do arquivo
+        try {
+            // Passo 5: Verificar se o arquivo é de texto
+            std::string line;
+            if (!std::getline(file, line)) {
+                throw NotATextFileException();
+            }
+
+            // Contagem de linhas, palavras e caracteres
+            int lineCount = 1;
+            int wordCount = 0;
+            int charCount = line.length();
+
+            while (std::getline(file, line)) {
+                lineCount++;
+                charCount += line.length();
+
+                // Contagem de palavras (considerando espaços como delimitadores)
+                bool inWord = false;
+                for (char c : line) {
+                    if (std::isspace(c)) {
+                        inWord = false;
+                    } else if (!inWord) {
+                        inWord = true;
+                        wordCount++;
+                    }
+                }
+            }
+
+            // Passo 6: Exibir informações
+            std::cout << "Número de linhas: " << lineCount << std::endl;
+            std::cout << "Número de palavras: " << wordCount << std::endl;
+            std::cout << "Número de caracteres: " << charCount << std::endl;
+        } catch (const NotATextFileException& e) {
+            std::cerr << "Erro: " << e.what() << std::endl;
+        } catch (const std::exception& e) {
+            std::cerr << "Erro ao processar o arquivo: " << e.what() << std::endl;
+        }
+
+        // Fechar o arquivo após o processamento
+        file.close();
+
+        // Se o programa chegar a este ponto, o arquivo existe e foi aberto com sucesso
+        std::cout << "Arquivo aberto e processado com sucesso." << std::endl;
+    } catch (const fs::filesystem_error& e) {
+        std::cerr << "Erro ao acessar o arquivo: " << e.what() << std::endl;
+    } catch (const std::ios_base::failure& e) {
+        std::cerr << "Erro ao abrir o arquivo: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
--- M3/W8/INF1900_M3_Exercicio_3/main.cpp
+++ M3/W8/INF1900_M3_Exercicio_3/main.cpp
@@ -1,5 +1,77 @@
-#include <iostream>
+/**
+ * @file main.cpp
+ *
+ * @brief Classe com função main para execução dos pontos da atividade.
+ *
+ * @author Rafael Taveira / Renan Alencar
+ * Contact: rafael.t@sidi.org.br / renan.a@sidi.org.br
+ *
+ */
 
-int main(int, char**){
-    std::cout << "Hello, from INF_1900_M3_Exercicio_3!\n";
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <utility>
+
+template <typename T>
+class FileResource {
+public:
+    // Construtor que abre o arquivo
+    FileResource(const std::string& filename) : file_(std::make_unique<std::fstream>(filename, std::ios::in | std::ios::binary)) {
+        if (!file_->is_open()) {
+            throw std::runtime_error("Não foi possível abrir o arquivo.");
+        }
+    }
+
+    // Método para ler e retornar os dados do arquivo
+    T ReadData() {
+        T data;
+        if (file_->read(reinterpret_cast<char*>(&data), sizeof(T))) {
+            return data;
+        } else {
+            throw std::runtime_error("Erro ao ler os dados do arquivo.");
+        }
+    }
+
+    // Método para escrever dados no arquivo
+    void WriteData(const T& data) {
+        if (file_->write(reinterpret_cast<const char*>(&data), sizeof(T))) {
+            file_->flush();
+        } else {
+            throw std::runtime_error("Erro ao escrever os dados no arquivo.");
+        }
+    }
+
+    // Método para mover o recurso de arquivo para outro objeto FileResource
+    void MoveTo(FileResource& other) {
+        other.file_ = std::move(file_);
+    }
+
+private:
+    std::unique_ptr<std::fstream> file_;
+};
+
+int main() {
+    try {
+        FileResource<int> fileInt("int_data.dat");
+        int data = fileInt.ReadData();
+        std::cout << "Dados lidos: " << data << std::endl;
+
+        FileResource<float> fileFloat("float_data.dat");
+        float newData = 42.5;
+        fileFloat.WriteData(newData);
+
+        // Movendo o recurso de arquivo de um objeto FileResource para outro
+        FileResource<float> movedResource("moved_data.dat");
+        fileFloat.MoveTo(movedResource);
+
+        float movedData = movedResource.ReadData();
+        std::cout << "Dados movidos: " << movedData << std::endl;
+
+    } catch (const std::exception& e) {
+        std::cerr << "Erro: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
